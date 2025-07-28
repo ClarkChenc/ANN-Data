5eb7a77e73e3cacdc0e49c79ef7621b3354581f4
clarkchenc <clarkchenc@qq.com>
2025-07-28 21:00:39 +0800

    mod umap

--- script/analyze.py
+++ script/analyze.py
@@ -135,8 +135,8 @@ def plot_data_with_umap():
         point_colors[labels == label] = colors[idx]
 
     t_start = time.time()
-    reducer = umap.UMAP(n_neighbors=64, min_dist=0.1,
-                        metric='cosine', random_state=42, n_jobs = 10)
+    reducer = umap.UMAP(n_components=3, n_neighbors=200, min_dist=0.1,
+                        metric='cosine', random_state=42, n_jobs=10)
     data_2d = reducer.fit_transform(data)
     t_end = time.time()
     print(f"cost: {t_end - t_start:.2f} s")
